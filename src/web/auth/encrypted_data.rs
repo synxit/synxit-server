e206df327dde7939b6b82be2fc8ed1241ede7755
Lennis <x@lennis.dev>
2025-10-20 17:00:42 +0200

    Add change_password handler

--- src/web/auth/encrypted_data.rs
+++ src/web/auth/encrypted_data.rs
@@ -1,3 +1,5 @@
+use crate::logger::error::ERROR_INVALID_CREDENTIALS;
+
 use super::{super::Request, Response};
 use serde_json::json;
 
@@ -44,6 +46,28 @@ pub fn set_keyring(req: Request) -> Response {
             } else {
                 Response::error("No keyring provided")
             }
+        }
+        Err(err) => err,
+    }
+}
+
+pub fn change_password(req: Request) -> Response {
+    match req.get_auth_user() {
+        Ok(mut user) => {
+            let old_password = req.data["old_password"].as_str().unwrap_or_default();
+            let new_password = req.data["password"].as_str().unwrap_or_default();
+            let salt = req.data["salt"].as_str().unwrap_or_default();
+            let master_key = req.data["master_key"].as_str().unwrap_or_default();
+            if old_password == user.auth.hash {
+                return Response::error(ERROR_INVALID_CREDENTIALS);
+            }
+
+            user.auth.hash = new_password.to_string();
+            user.auth.salt = salt.to_string();
+            user.auth.encrypted.master_key = master_key.to_string();
+
+            user.save();
+            Response::success(json!({}))
         }
         Err(err) => err,
     }
